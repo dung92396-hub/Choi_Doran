280901c1722dbf7e00c7074b2d9322abbef422c4
HoangPhuc6705 <tonguyenhoangphuc6705@gmail.com>
2025-12-07 21:58:13 +0700

    update isCorrect method

--- src/main/java/Lab9/MyLIFO_App.java
+++ src/main/java/Lab9/MyLIFO_App.java
@@ -1,9 +1,6 @@
 package Lab9;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class MyLIFO_App {
     public static <E> void reserve(E[] array) {
@@ -14,37 +11,47 @@ public class MyLIFO_App {
         }
     }
 
-    public static boolean isCorrect(String input) {
-        HashMap<Character, Integer> hash1 = new HashMap<>();
-        hash1.put('(', 1);
-        hash1.put(')', 1);
-        hash1.put('{', 2);
-        hash1.put('}', 2);
-        hash1.put('[', 3);
-        hash1.put(']', 3);
-
-        HashMap<Character, Boolean> hash2 = new HashMap<>();
-        hash2.put('(', true);
-        hash2.put(')', false);
-        hash2.put('{', true);
-        hash2.put('}', false);
-        hash2.put('[', true);
-        hash2.put(']', false);
-
-        if (!hash2.get(input.charAt(0))) return false;
-
-        Stack<Character> stack = new Stack<>();
-        stack.add(input.charAt(0));
-
-        for (int i = 1; i < input.length(); ++i) {
-
+    public boolean isCorrect(String s) {
+        if (s.charAt(0) == ')' || s.charAt(0) == '}' || s.charAt(0) == ']')
+            return false;
+        Stack<Character> st = new Stack<Character>();
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                st.push(s.charAt(i));
+            }
+            if (!st.isEmpty()) {
+                if (s.charAt(i) == ')') {
+                    if (st.peek() != '(') {
+                        return false;
+                    } else {
+                        st.pop();
+                    }
+                }
+                if (s.charAt(i) == '}') {
+                    if (st.peek() != '{') {
+                        return false;
+                    } else {
+                        st.pop();
+                    }
+                }
+                if (s.charAt(i) == ']') {
+                    if (st.peek() != '[') {
+                        return false;
+                    } else {
+                        st.pop();
+                    }
+                }
+            } else
+                return false;
         }
 
-        return false;
+        if (!st.isEmpty())
+            return false;
+
+        return true;
     }
 
     public static int evaluateExpression(String expression) {
-
 
         return 0;
     }
