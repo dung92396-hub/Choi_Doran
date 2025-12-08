1608ff95864029a228ee6bb5271906c8a452813b
giahuy362006-create <giahuy362005@gmail.com>
2025-12-08 14:01:22 +0700

    update calc function

--- src/main/java/Lab9/MyLIFO_App.java
+++ src/main/java/Lab9/MyLIFO_App.java
@@ -52,8 +52,73 @@ public class MyLIFO_App {
     }
 
     public static int evaluateExpression(String expression) {
+        Stack < Integer > values = new Stack < > ();
+        Stack < Character > operators = new Stack < > ();
 
-        return 0;
+        int i = 0;
+        while (i < expression.length()) {
+            char currentChar = expression.charAt(i);
+
+            if (currentChar == ' ') {
+                i++;
+                continue;
+            }
+
+            if (Character.isDigit(currentChar)) {
+                int num = 0;
+                while (i < expression.length() && Character.isDigit(expression.charAt(i))) {
+                    num = num * 10 + (expression.charAt(i) - '0');
+                    i++;
+                }
+                values.push(num);
+            } else if (currentChar == '(') {
+                operators.push(currentChar);
+                i++;
+            } else if (currentChar == ')') {
+                while (operators.peek() != '(') {
+                    values.push(applyOperator(operators.pop(), values.pop(), values.pop()));
+                }
+                operators.pop();
+                i++;
+            } else if (currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/') {
+                while (!operators.isEmpty() && hasPrecedence(currentChar, operators.peek())) {
+                    values.push(applyOperator(operators.pop(), values.pop(), values.pop()));
+                }
+                operators.push(currentChar);
+                i++;
+            }
+        }
+
+        while (!operators.isEmpty()) {
+            values.push(applyOperator(operators.pop(), values.pop(), values.pop()));
+        }
+
+        return values.pop();
+    }
+
+    public static int applyOperator(char operator, int b, int a) {
+        switch (operator) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                return a / b;
+            default:
+                return 0;
+        }
+    }
+
+    public static boolean hasPrecedence(char operator1, char operator2) {
+        if (operator2 == '(' || operator2 == ')') {
+            return false;
+        }
+        if ((operator1 == '*' || operator1 == '/') && (operator2 == '+' || operator2 == '-')) {
+            return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
