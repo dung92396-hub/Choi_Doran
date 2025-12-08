a39b09264c680216925184ec5df23d8e056959bf
HoangPhuc6705 <tonguyenhoangphuc6705@gmail.com>
2025-12-08 13:36:39 +0700

    gia heo ne

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,11 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(List.of(6, 3, 1, 8, 4));
-        System.out.println(list.stream().max((a, b) -> a.compareTo(b)));
+        List<Integer> arr = new ArrayList<>(List.of(11, 4, 3, 8, 5, 6));
     }
-
 }
