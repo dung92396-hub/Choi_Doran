b89b2f79a39e05e52af12bd3e0ce1ae68fa251f3
HoangPhuc6705 <tonguyenhoangphuc6705@gmail.com>
2025-12-11 22:48:18 +0700

    java stream update example

--- src/main/java/java_stream/Main.java
+++ src/main/java/java_stream/Main.java
@@ -1,22 +1,28 @@
 package java_stream;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 public class Main {
+    static void testArrayStream() {
+        ArrayList<Integer> arr = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
+        // tong cac phan tu, identity: gia tri khoi tao, (a, b) la gia tri tiep theo trong mang
+        int sum = arr.stream().reduce(0, (a, b) -> a + b);
+//        int sum = arr.stream().reduce(0, Integer::sum); co the viet dang nay cho gon
+
+        // tich cac phan tu
+        int mul = arr.stream().reduce(1, (a, b) -> a * b);
+
+
+        // tong cac phan tu le
+        int sumOfEven = arr.stream().filter(e -> e % 2 == 0).reduce(0, Integer::sum);
+
+        System.out.println(sum);
+        System.out.println(mul);
+        System.out.println(sumOfEven);
+    }
 
     public static void main(String[] args) {
-        ArrayList<Book> books = new ArrayList<>();
-        books.add(new Book("Triet hoc Mac Lenin", 400, 2016));
-        books.add(new Book("Cau truc roi rac", 120, 2013));
-        books.add(new Book("UX/UI", 200, 2023));
-        books.add(new Book("Giai tich 1", 600, 2008));
-
-
-        // stream sort
-        System.out.println(books.stream().sorted(Comparator.comparingInt(Book::getPublishYear)).toList());
+        testArrayStream();
     }
 }
