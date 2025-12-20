f68ebe1a625683694f4418a1127d9470b1c16b3d
HoangPhuc6705 <tonguyenhoangphuc6705@gmail.com>
2025-12-20 12:41:36 +0700

    update java8_pack

---
+++ src/main/java/Java8_pack/AnimalManger/HeathState.java
@@ -0,0 +1,4 @@
+package Java8_pack.AnimalManger;
+
+public enum HeathState {
+}
---
+++ src/main/java/Java8_pack/AnimalManger/Pet.java
@@ -0,0 +1,6 @@
+package Java8_pack.AnimalManger;
+
+public class Animal {
+    private String name;
+    private String type;
+}
